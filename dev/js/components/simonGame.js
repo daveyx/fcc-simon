4ad2cfa44283d42539e6917783ef3b0cb6191c1d
daveyx <d.rehle@gmail.com>
2017-05-09 19:09:54 +0200

    added start,stop,restart buttons

--- dev/js/components/simonGame.js
+++ dev/js/components/simonGame.js
@@ -1,23 +1,77 @@
 'use strict';
 
 import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
 import '../../css/styles.css';
+import Control from './control';
 
 export default class SimonGame extends Component {
   constructor(props) {
     super();
     this.state = {
+      strictMode: false,
+      gameRunning: false
     };
+    this.toggleStrictMode = this.toggleStrictMode.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.stopGame = this.stopGame.bind(this);
+    this.restartGame = this.restartGame.bind(this);
+  }
+
+  startGame() {
+    this.setState({
+      gameRunning: true
+    });
+  }
+
+  stopGame() {
+    this.setState({
+      gameRunning: false
+    });
+  }
+
+  restartGame() {
+
+  }
+
+  toggleStrictMode() {
+    if (this.state.strictMode) {
+      this.setState({
+        strictMode: false
+      });
+    } else {
+      this.setState({
+        strictMode: true
+      });
+    }
   }
 
   render() {
     return(
-      <div className="simon">
-        <div className="pad red" ></div>
-        <div className="pad green" ></div>
-        <div className="pad yellow" ></div>
-        <div className="pad blue" ></div>
-        <div className="display" id="display"></div>
+      <div>
+        <Row>
+          <Col xs={12}>
+            <div className="simon">
+              <div className="pad red" ></div>
+              <div className="pad green" ></div>
+              <div className="pad yellow" ></div>
+              <div className="pad blue" ></div>
+              <div className="display" id="display"></div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Control
+              strictMode={this.state.strictMode}
+              toggleStrictMode={this.toggleStrictMode}
+              gameRunning={this.state.gameRunning}
+              startGame={this.startGame}
+              stopGame={this.stopGame}
+              restartGame={this.restartGame}
+            />
+          </Col>
+        </Row>
       </div>
     );
   }
